60610fb3a08bb9eaa743edd793fb724b8d6c3bab
Franklin <franko4don@yahoo.com>
2024-06-21 19:37:06 +0100

    UI customization (#2)
    * chores: implemented UI changes and updated rpc and testnet links
    
    * added new rpcs links
    
    * qa fixes
    
    ---------
    
    Co-authored-by: James Harrison <somtochukwujames2000@gmail.com>
    Co-authored-by: Николай Скиба <nikolajskiba@Nikolajs-MacBook-Pro.local>

--- frontend/src/store/business/token.ts
+++ frontend/src/store/business/token.ts
@@ -7,20 +7,26 @@ export const useTokenRead = () => {
 
   return {
     balanceToken: (chainId: ChainId) => {
-      return token.balances[chainId]?.formatString(token.decimals, 18) ?? '0'
+      return token.balances[chainId]?.formatString(token.decimals[chainId], 18) ?? '0'
+    },
+    balanceBridge: (chainId: ChainId) => {
+      return (
+        token.contractBalances[chainId]?.formatString(token.decimals[chainId], 18) ?? '0'
+      )
     },
     balanceTokenNumber: (chainId: ChainId) => {
-      return token.balances[chainId]?.formatNumber(token.decimals, 18) ?? 0
+      return token.balances[chainId]?.formatNumber(token.decimals[chainId], 18) ?? 0
     },
     balanceNative: (chainId: ChainId) => {
-      return token.balancesNative[chainId]?.formatNumber(token.decimals, 5) ?? 0
+      return token.balancesNative[chainId]?.formatNumber(token.decimals[chainId], 5) ?? 0
     },
     addresses: (chainId: ChainId) => computed(() => token.tokens[chainId]),
     allowance: (chainId: ChainId) => token.allowances[chainId],
     symbol: computed(() => token.symbol),
     getCurrentSymbol: (tokenAddres: string) => computed(() => token.cSymbol[tokenAddres]),
     decimals: computed(() => token.decimals),
-    getCurrentDecimals: (tokenAddres: string) => computed(() => token.cDecimals[tokenAddres]),
+    getCurrentDecimals: (tokenAddres: string) =>
+      computed(() => token.cDecimals[tokenAddres]),
     tokens: computed(() => token.tokens),
   }
 }
