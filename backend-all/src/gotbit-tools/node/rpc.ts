90922f8f607f14c2de5b5e5333c9e8d8e5ca41a7
stephenson080 <stevepathagoras08@gmail.com>
2025-03-25 10:18:14 +0100

    Init backend restructure

---
+++ backend-all/src/gotbit-tools/node/rpc.ts
@@ -0,0 +1,120 @@
+import { ChainTag, RemoteChainTag, RpcFunction } from './utils/misc'
+import { extraRpcs } from './utils/node'
+
+export const ankrRpc = (): RpcFunction => {
+  const ankrPath: Partial<Record<ChainTag, string>> = {
+    avax_mainnet: '/avalanche',
+    bsc_mainnet: '/bsc',
+    arbitrum_mainnet: '/arbitrum',
+    eth_mainnet: '/eth',
+    ftm_mainnet: '/fantom',
+    polygon_mainnet: '/polygon',
+    celo_mainnet: '/celo',
+
+    avax_testnet: '/avalanche_fuji',
+    polygon_testnet: '/polygon_mumbai',
+    ftm_testnet: '/fantom_testnet',
+    rinkeby: '/eth_rinkeby',
+    ropsten: '/eth_ropsten',
+    goerli: '/eth_goerli',
+  }
+  return (chainTag: ChainTag) => {
+    if (chainTag === 'pulse_mainnet') return 'https://pulsechain.publicnode.com'
+    if (chainTag === 'pulse_testnet') return 'https://rpc.v4.testnet.pulsechain.com'
+    if (chainTag === 'base_mainnet') return 'https://base-rpc.publicnode.com'
+    if (chainTag === 'arbitrum_sepolia') return 'https://sepolia-rollup.arbitrum.io/rpc'
+    if (chainTag === 'xend_testnet') return 'https://enugu-rpc.assetchain.org'
+    if (chainTag === 'eth_sepolia') return 'https://ethereum-sepolia-rpc.publicnode.com'
+    if (chainTag === 'polygon_amoy') return 'https://rpc-amoy.polygon.technology'
+    if (chainTag === 'base_sepolia') return 'https://rpc.notadegen.com/base/sepolia'
+
+    if (chainTag === 'avax_testnet')
+      return 'https://avalanche-fuji-c-chain.publicnode.com'
+
+    return 'https://rpc.ankr.com' + ankrPath[chainTag] ?? ''
+  }
+}
+
+const goodRpcProvider = [ankrRpc()]
+
+export const extraRpc = (indexes?: Partial<Record<ChainTag, number>>) => {
+  return (chainTag: ChainTag) => {
+    let index = 0
+    if (indexes) {
+      const possibleIndex = indexes[chainTag]
+      if (possibleIndex !== undefined) index = possibleIndex
+    }
+
+    const rpcList = (extraRpcs as any)[chainTag] ?? []
+
+    for (const goodRpc of goodRpcProvider) {
+      const rpc = goodRpc(chainTag)
+      if (rpc) rpcList.push(rpc)
+    }
+
+    return index < rpcList.length ? rpcList[index] : ''
+  }
+}
+
+export const universalRpc = (): RpcFunction => {
+  const ankr = ankrRpc()
+  return (chainTag: ChainTag) => {
+    const a: Record<ChainTag, string> = {
+      avax_mainnet: ankr(chainTag),
+      bsc_mainnet: ankr(chainTag),
+      arbitrum_mainnet: ankr(chainTag),
+      eth_mainnet: ankr(chainTag),
+      ftm_mainnet: ankr(chainTag),
+      polygon_mainnet: ankr(chainTag),
+      celo_mainnet: ankr(chainTag),
+      base_mainnet: ankr(chainTag),
+      bitlayer_mainnet: extraRpcs.bitlayer_mainnet[0],
+      xend_mainnet: extraRpcs.xend_mainnet[0],
+
+      avax_testnet: 'https://avalanche-fuji-c-chain.publicnode.com',
+      polygon_testnet: 'https://rpc-mumbai.maticvigil.com',
+      ftm_testnet: ankr(chainTag),
+      rinkeby: ankr(chainTag),
+      ropsten: ankr(chainTag),
+      goerli: ankr(chainTag),
+
+      arbitrum_testnet: extraRpcs.arbitrum_testnet[0],
+      // bsc_testnet: extraRpcs.bsc_testnet[0],
+      bsc_testnet: 'https://bsc-testnet.publicnode.com',
+
+      localhost: extraRpcs.localhost[0],
+
+      metis_mainnet: extraRpcs.metis_mainnet[0],
+      cube_mainnet: extraRpcs.cube_mainnet[0],
+      okex_mainnet: extraRpcs.okex_mainnet[0],
+      cmp_mainnet: extraRpcs.cmp_mainnet[0],
+      pulse_mainnet: 'https://pulsechain.publicnode.com',
+
+      celo_alfajores_testnet: extraRpcs.celo_alfajores_testnet[0],
+      metis_testnet: extraRpcs.metis_testnet[0],
+      cube_testnet: extraRpcs.cube_testnet[0],
+      okex_testnet: extraRpcs.okex_testnet[0],
+      cmp_testnet: extraRpcs.cmp_testnet[0],
+      pulse_testnet: 'https://rpc.v4.testnet.pulsechain.com',
+      arbitrum_sepolia: extraRpcs.arbitrum_sepolia[0],
+      xend_testnet: extraRpcs.xend_testnet[0],
+      base_sepolia: extraRpcs.base_sepolia[0],
+      eth_sepolia: extraRpcs.eth_sepolia[0],
+      polygon_amoy: extraRpcs.polygon_amoy[0],
+      bitlayer_testnet: extraRpcs.bitlayer_testnet[0],
+    } as any
+    return a[chainTag]
+  }
+}
+
+export const toRealChainTag = (chainTag: ChainTag) => {
+  return process.env.MODE ? chainTag + '_' + process.env.MODE : chainTag
+}
+
+export const remoteRpc = (tags: Partial<Record<RemoteChainTag, string>>): RpcFunction => {
+  return (chainTag: ChainTag) => {
+    const remote = tags[chainTag as RemoteChainTag]
+    if (remote) return remote
+    return universalRpc()(chainTag)
+  }
+}
---
+++ backend-all/src/services/blockchain.ts
@@ -0,0 +1,98 @@
+import { Wallet, ethers, BigNumber, BigNumberish } from 'ethers'
+import { useContracts, getProvider, safeRead } from '@/gotbit-tools/node'
+import { config } from '@/gotbit.config'
+import { ChainId } from '@/gotbit-tools/node/types'
+import CONFIRMATIONS from '../confirmations.json'
+import { FulfillTxContract, TransactionContract } from '@/types'
+import { EIP712DOMAIN_NAME, EIP712DOMAIN_VERSION, eip712Transaction } from '@/utils/constant'
+
+
+
+export const getWalletEVM = () => new Wallet(process.env.PRIVATE_KEY!)
+
+const getWallet = (signerIndex: number) => {
+  if (signerIndex === 0) {
+    return new Wallet(process.env.PRIVATE_KEY, getProvider(config.DEFAULT_CHAINID))
+  }
+  return new Wallet(process.env.PRIVATE_KEY2!, getProvider(config.DEFAULT_CHAINID))
+}
+
+
+async function signHashedTransaction(
+  transaction: FulfillTxContract,
+  chainId: string,
+  contractAddress: string,
+  signerIndex: number
+) {
+  const wallet = getWallet(signerIndex)
+  const domain = {
+    name: EIP712DOMAIN_NAME,
+    version: EIP712DOMAIN_VERSION,
+    chainId,
+    verifyingContract: contractAddress,
+  }
+  const types = {
+    [eip712Transaction.name]: eip712Transaction.fields,
+  }
+
+  const sign = await wallet._signTypedData(domain, types, transaction)
+  return sign
+}
+
+
+const extractFulfillTransaction = (tx: TransactionContract) => {
+  return {
+    amount: tx.amount.toString(),
+    fromChain: tx.fromChain.toString(),
+    nonce: tx.nonce.toString(),
+    fromUser: tx.fromUser.toString(),
+    toUser: tx.toUser.toString(),
+  }
+}
+
+export const signTransaction = async (
+  fromBridgeAddress: string,
+  toBridgeAddress: string,
+  fromChain: string,
+  fromUser: string,
+  index: string
+) => {
+  let tx: TransactionContract
+  if (fromChain.startsWith('evm.')) {
+    const { bridgeAssist } = useContracts(undefined, fromChain.slice(4) as ChainId)
+    tx = await bridgeAssist(fromBridgeAddress).transactions(fromUser, index)
+  } else {
+    throw Error('bad arguments')
+  }
+  const provider = getProvider(fromChain.slice(4) as ChainId)
+  const currentBlock = await safeRead(provider.getBlockNumber(), 0)
+  if (
+    currentBlock === 0 ||
+    tx.block.gt(currentBlock)
+  )
+    throw Error('waiting for confirmations')
+  if (tx.toChain.startsWith('evm.')) {
+    const chainId = tx.toChain.replace('evm.', '')
+    // const { bridgeAssist } = useContracts(undefined, chainId as ChainId)
+    let signatures : any[] = []
+    const signer0 = await signHashedTransaction(
+      extractFulfillTransaction(tx),
+      chainId,
+      toBridgeAddress,
+      0
+    )
+    signatures.push(signer0)
+    if (process.env.PRIVATE_KEY2){
+      const signer1 = await signHashedTransaction(
+        extractFulfillTransaction(tx),
+        chainId,
+        toBridgeAddress,
+        1
+      )
+      signatures.push(signer1)
+    }
+    return signatures
+  } else {
+    throw Error('bad contract params')
+  }
+}
