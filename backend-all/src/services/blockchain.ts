b7008dca8f9683cdc3acb96893b1bf5ba1b63c17
Franklin <franko4don@yahoo.com>
2025-05-09 15:37:38 +0100

    fix bug (#78)
    * Init backend restructure
    
    * added condition for other relayers
    
    * added condition for other relayers
    
    * added condition for other relayers
    
    * [Update]: Update backend code
    
    * Create parse-apps2.config.js
    
    * Create parse-apps3.config.js
    
    * [Update]: Changed to use structure
    
    * new-structure
    
    * changed rpc for arb testnet
    
    * changed rpc for arb testnet
    
    * change rpc
    
    * change rpc
    
    * Update push-prod.yml
    
    * added IP whitelist (#75)
    
    Co-authored-by: Franklin Nwanze <franko4don@yahoo.com>
    
    * Update blockchain.ts
    
    * Update blockchain.ts
    
    ---------
    
    Co-authored-by: stephenson080 <stevepathagoras08@gmail.com>
    Co-authored-by: Stephen Udeh <65906366+stephenson080@users.noreply.github.com>

--- backend-all/src/services/blockchain.ts
+++ backend-all/src/services/blockchain.ts
@@ -77,7 +77,8 @@ export const signTransaction = async (
     const chainId = tx.toChain.replace('evm.', '')
     // const { bridgeAssist } = useContracts(undefined, chainId as ChainId)
     let signatures: any[] = []
-    if (process.env.IS_PUBLIC_RELAYER === 'false'{
+    
+    if (process.env.IS_PUBLIC_RELAYER === 'false'){
       if (!allowedIps?.includes(clientIp)) {
         throw new Error('IP not allowed to connect to the relayer')
       }
