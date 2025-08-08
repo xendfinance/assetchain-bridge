ee25c6c9fc92b064b31b4ca821c95ac816f56ed3
Stephen Udeh <stevepathagoras08@gmail.com>
2025-08-08 14:41:08 +0100

    use polygon rpc from env

--- backend-all/src/gotbit.config.ts
+++ backend-all/src/gotbit.config.ts
@@ -4,7 +4,7 @@ import { universalRpc } from '@/gotbit-tools/node/rpc'
 export const IS_PROD = process.env.PROD === 'true'
 
 export const config = defineConfig({
-  chainIds: ['97', '42421', '421614', '200810', '200901', '42420', '42161', '56', '8453', '84532', "1", '137'],
+  chainIds: ['97', '42421', '421614', '200810', '200901', '42420', '42161', '56', '8453', '84532', "1", '137', '80002'],
   // DEFAULT_CHAINID: IS_PROD ? '56' : '97',
   DEFAULT_CHAINID: '97',
   rpc: universalRpc(),
@@ -27,5 +27,6 @@ export const contracts = defineContracts({
   '8453': {},
   '84532': {},
   "1": {},
-  "137": {}
+  "137": {},
+  "80002": {}
 })
