c78ed4b63827f90815177cb0d9caef603b8e9c79
Stephen Udeh <65906366+stephenson080@users.noreply.github.com>
2025-08-08 17:29:01 +0100

    polygon support (#86)
    * Main (#81)
    
    * fix var issue (#80)
    
    * Init backend restructure
    
    * added condition for other relayers
    
    * added condition for other relayers
    
    * added condition for other relayers
    
    * [Update]: Update backend code
    
    * Create parse-apps2.config.js
    
    * Create parse-apps3.config.js
    
    * [Update]: Changed to use structure
    
    * new-structure
    
    * changed rpc for arb testnet
    
    * changed rpc for arb testnet
    
    * change rpc
    
    * change rpc
    
    * Update push-prod.yml
    
    * added IP whitelist (#75)
    
    Co-authored-by: Franklin Nwanze <franko4don@yahoo.com>
    
    * Update blockchain.ts
    
    * Update blockchain.ts
    
    * fix bug (#78) (#79)
    
    * Init backend restructure
    
    * added condition for other relayers
    
    * added condition for other relayers
    
    * added condition for other relayers
    
    * [Update]: Update backend code
    
    * Create parse-apps2.config.js
    
    * Create parse-apps3.config.js
    
    * [Update]: Changed to use structure
    
    * new-structure
    
    * changed rpc for arb testnet
    
    * changed rpc for arb testnet
    
    * change rpc
    
    * change rpc
    
    * Update push-prod.yml
    
    * added IP whitelist (#75)
    
    
    
    * Update blockchain.ts
    
    * Update blockchain.ts
    
    ---------
    
    Co-authored-by: stephenson080 <stevepathagoras08@gmail.com>
    Co-authored-by: Stephen Udeh <65906366+stephenson080@users.noreply.github.com>
    
    * Update blockchain.ts
    
    ---------
    
    Co-authored-by: stephenson080 <stevepathagoras08@gmail.com>
    Co-authored-by: Stephen Udeh <65906366+stephenson080@users.noreply.github.com>
    
    * log to check error
    
    * changes to backend
    
    * added deploy script
    
    * added deploy script
    
    ---------
    
    Co-authored-by: stephenson080 <stevepathagoras08@gmail.com>
    Co-authored-by: Stephen Udeh <65906366+stephenson080@users.noreply.github.com>
    
    * worked on multiple rpc
    
    * Rpc (#82)
    
    * fix var issue (#80)
    
    * Init backend restructure
    
    * added condition for other relayers
    
    * added condition for other relayers
    
    * added condition for other relayers
    
    * [Update]: Update backend code
    
    * Create parse-apps2.config.js
    
    * Create parse-apps3.config.js
    
    * [Update]: Changed to use structure
    
    * new-structure
    
    * changed rpc for arb testnet
    
    * changed rpc for arb testnet
    
    * change rpc
    
    * change rpc
    
    * Update push-prod.yml
    
    * added IP whitelist (#75)
    
    Co-authored-by: Franklin Nwanze <franko4don@yahoo.com>
    
    * Update blockchain.ts
    
    * Update blockchain.ts
    
    * fix bug (#78) (#79)
    
    * Init backend restructure
    
    * added condition for other relayers
    
    * added condition for other relayers
    
    * added condition for other relayers
    
    * [Update]: Update backend code
    
    * Create parse-apps2.config.js
    
    * Create parse-apps3.config.js
    
    * [Update]: Changed to use structure
    
    * new-structure
    
    * changed rpc for arb testnet
    
    * changed rpc for arb testnet
    
    * change rpc
    
    * change rpc
    
    * Update push-prod.yml
    
    * added IP whitelist (#75)
    
    
    
    * Update blockchain.ts
    
    * Update blockchain.ts
    
    ---------
    
    Co-authored-by: stephenson080 <stevepathagoras08@gmail.com>
    Co-authored-by: Stephen Udeh <65906366+stephenson080@users.noreply.github.com>
    
    * Update blockchain.ts
    
    ---------
    
    Co-authored-by: stephenson080 <stevepathagoras08@gmail.com>
    Co-authored-by: Stephen Udeh <65906366+stephenson080@users.noreply.github.com>
    
    * log to check error
    
    * changes to backend
    
    * added deploy script
    
    * added deploy script
    
    * add deploy script for xRWA
    
    * checking error
    
    * removed logs
    
    * [chores] use multiple rpc
    
    * worked on multiple rpc
    
    ---------
    
    Co-authored-by: Franklin <franko4don@yahoo.com>
    
    * polygon support
    
    * polygon support (#84)
    
    * use polygon rpc from env
    
    ---------
    
    Co-authored-by: Franklin <franko4don@yahoo.com>

--- backend-all/src/utils/helpers.ts
+++ backend-all/src/utils/helpers.ts
@@ -91,6 +91,10 @@ export function getChainRPCS(chainId: ChainId) {
       return binance_testnet_rpc
     case '1':
       return ethereum_mainnet_rpc
+    case '137':
+        return polygon_mainnet_rpc
+    case '80002':
+        return polygon_amoy_rpc
     default:
       return undefined
   }
